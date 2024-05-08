a0aa0780e226ef03b9458764bddad1fa10da0d1e
Dodas_ <ph005009004@gmail.com>
2024-05-08 00:29:56 -0300

    fix: slideshow display

--- site/index/index.js
+++ site/index/index.js
@@ -1,18 +1,37 @@
-let slideIndex = 1;
+var slideIndex = 1;
+var prevButton = document.querySelector('.prev');
+var nextButton = document.querySelector('.next');
+const slides = document.getElementsByClassName('slide-content');
+const dots = document.getElementsByClassName('dot');
 showSlide(slideIndex);
 
-function changeSlide(n) {
-
+prevButton.onclick = () => {
+    slideIndex--;
+    showSlide(slideIndex);
 }
 
-function currentSlide(n) {
-
+nextButton.onclick = () => {
+    slideIndex++;
+    showSlide(slideIndex);
 }
 
 function showSlide(n) {
     let i;
-    let slides = document.getElementsByClassName("slide-content");
-    let dots = document.getElementsByClassName("dot");
-    if(n > slide.lenght) {slideIndex = 1;}
-    if(n < 1) {slideIndex = slides.length;}
+    if(n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if(n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for(i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for(i = 0; i < dots.length; i++) {
+        dots[i].classList.remove('active');
+    }
+    slides[slideIndex - 1].style.display = "flex";
+    dots[slideIndex - 1].classList.add('active');
 }
