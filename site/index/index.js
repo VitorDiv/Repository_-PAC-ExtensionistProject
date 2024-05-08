d0c57c9facceb305ca74104d03adc5a2f57c8e18
Dodas_ <53160578+PedRo-HenRique-14@users.noreply.github.com>
2024-05-07 22:00:11 -0300

    feat: adiciona slideshow ao index

---
+++ site/index/index.js
@@ -0,0 +1,18 @@
+let slideIndex = 1;
+showSlide(slideIndex);
+
+function changeSlide(n) {
+
+}
+
+function currentSlide(n) {
+
+}
+
+function showSlide(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide-content");
+    let dots = document.getElementsByClassName("dot");
+    if(n > slide.lenght) {slideIndex = 1;}
+    if(n < 1) {slideIndex = slides.length;}
+}
