e3ba23de679baf757982a3cf642d3a2a8e7df6fd
Dodas_ <ph005009004@gmail.com>
2024-05-09 00:44:05 -0300

    fix: modifica a funcionalidade do menu
    Agora para expandir o sub menu é necessário clicar em cima da seta. Ao clicar no span, você é redirecionado para a respectiva página

--- site/index/js/dropdown-menu.js
+++ site/index/js/dropdown-menu.js
@@ -1,10 +1,27 @@
 const menuItem = document.querySelectorAll('.menu-item');
-const menuItemTitle = document.querySelectorAll('.menu-item-title');
+const arrow = document.querySelectorAll('.arrow');
 
-menuItemTitle.forEach(title => {
-    title.addEventListener('click', () => {
+/*
+menuItem.forEach(item => {
+    item.addEventListener('mouseover', () => {
+        const body = item.querySelector('.sub-menu-body');
+        body.classList.toggle('sub-active');
+        const arrow = item.querySelector('.arrow');
+        arrow.classList.toggle('arrow-active');
+    });
+    item.addEventListener('mouseout', () => {
+        const body = item.querySelector('.sub-menu-body');
+        body.classList.toggle('sub-active');
+        const arrow = item.querySelector('.arrow');
+        arrow.classList.toggle('arrow-active');
+    });
+});
+*/
+
+arrow.forEach(arrow => {
+    arrow.addEventListener('click', () => {
         menuItem.forEach(item => {
-            if(item.querySelector('.menu-item-title') == title) {
+            if(item.querySelector('.arrow') == arrow) {
                 const body = item.querySelector('.sub-menu-body');
                 body.classList.toggle('sub-active');
                 const arrow = item.querySelector('.arrow');
